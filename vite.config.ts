a7044ae9dc97a05f259bd7a250424654858b6128
hikh703 <emrick.poncet@noet-ia.com>
2024-12-06 19:35:24 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EBCG/', // Replace with your repository name
+  base: 'https://github.com/hikh703/EBCG', // Replace with your repository name
   plugins: [
     vue(),
     vueJsx(),
